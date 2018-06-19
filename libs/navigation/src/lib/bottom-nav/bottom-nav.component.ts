0c9f36caac08b7646ce188ee0f755ddb08a58dc7
Andrew Kemp <andrew@ankemp.com>
2018-06-19 18:25:08 -0400

    Adding window-ref service

--- libs/navigation/src/lib/bottom-nav/bottom-nav.component.ts
+++ libs/navigation/src/lib/bottom-nav/bottom-nav.component.ts
@@ -3,6 +3,7 @@ import { ScrollDispatcher, CdkScrollable } from '@angular/cdk/scrolling';
 
 import { Observable } from 'rxjs';
 import { map, pairwise } from 'rxjs/operators';
+import { WindowRef } from '../window-ref.service';
 import { MenuItem } from '../../models';
 
 @Component({
@@ -13,15 +14,20 @@ import { MenuItem } from '../../models';
 export class BottomNavComponent implements OnInit {
   @Input() menuItems: MenuItem[];
   private scrollEvent: Observable<CdkScrollable | void>;
+  private nativeWindow: Window;
   pageYOffset: Observable<number>;
   showNav = true;
 
-  constructor(private dispatcher: ScrollDispatcher) {
+  constructor(
+    private dispatcher: ScrollDispatcher,
+    windowRef: WindowRef
+  ) {
     this.scrollEvent = this.dispatcher.scrolled();
+    this.nativeWindow = windowRef.nativeWindow;
   }
 
   ngOnInit() {
-    this.pageYOffset = this.scrollEvent.pipe(map(_ => window.pageYOffset));
+    this.pageYOffset = this.scrollEvent.pipe(map(_ => this.nativeWindow.pageYOffset));
     this.pageYOffset.pipe(pairwise()).subscribe(([newYOffset, oldYOffset]) => {
       this.showNav = (newYOffset >= oldYOffset);
     });
