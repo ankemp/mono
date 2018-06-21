18dc80e5ff92344f392f95aaca645b8041055e4a
Andrew Kemp <andrew@ankemp.com>
2018-06-21 15:01:08 -0400

    Scaffolding ui-state lib

---
+++ libs/ui-state/src/lib/ui-state.module.spec.ts
@@ -0,0 +1,16 @@
+import { async, TestBed } from '@angular/core/testing';
+import { UiStateModule } from './ui-state.module';
+
+describe('UiStateModule', () => {
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [UiStateModule]
+      }).compileComponents();
+    })
+  );
+
+  it('should create', () => {
+    expect(UiStateModule).toBeDefined();
+  });
+});
