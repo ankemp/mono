0a82654d157a3f2a95e7182009a578a301e0a486
Andrew Kemp <andrew@ankemp.com>
2018-06-21 16:57:32 -0400

    Scaffolding notification banner lib

--- libs/fb-user-profile/src/lib/fb-user-profile.module.ts
+++ libs/fb-user-profile/src/lib/fb-user-profile.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { UserProfileRoutingModule } from './user-profile.routing';
+import { UserProfileRoutingModule } from './fb-user-profile-routing.module';
 import { ProfileComponent } from './profile/profile.component';
 import { PublicProfileComponent } from './public-profile/public-profile.component';
 import { ManageProfileComponent } from './manage-profile/manage-profile.component';
---
+++ libs/notification-banner/src/lib/notification-banner.module.spec.ts
@@ -0,0 +1,16 @@
+import { async, TestBed } from '@angular/core/testing';
+import { NotificationBannerModule } from './notification-banner.module';
+
+describe('NotificationBannerModule', () => {
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [NotificationBannerModule]
+      }).compileComponents();
+    })
+  );
+
+  it('should create', () => {
+    expect(NotificationBannerModule).toBeDefined();
+  });
+});
