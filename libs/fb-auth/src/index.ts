7733c9916f36b5a9e9e64b2ffbe19928635e3e62
Andrew Kemp <andrew@ankemp.com>
2018-07-12 00:27:56 -0400

    Exporting fb-auth state

--- libs/fb-auth/src/index.ts
+++ libs/fb-auth/src/index.ts
@@ -1,2 +1,5 @@
 export * from './lib/fb-auth.module';
 export * from './lib/services/auth.service';
+export * from './lib/state';
+export * from './lib/state/auth.actions';
+export { State as AuthState } from './lib/state/auth.reducer';
