9b75a000ac202f4c10689e072a6dbccd9f049ed6
Andrew Kemp <andrew@ankemp.com>
2018-07-13 13:36:03 -0400

    fb-auth lib: Adding missing exports & cleanup

--- libs/fb-auth/src/index.ts
+++ libs/fb-auth/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/fb-auth.module';
-export * from './lib/services/auth.service';
+export * from './models';
+export * from './lib/services';
 export * from './lib/state';
 export * from './lib/state/auth.actions';
 export { State as AuthState } from './lib/state/auth.reducer';
--- libs/fb-auth/src/lib/state/auth.effects.ts
+++ libs/fb-auth/src/lib/state/auth.effects.ts
@@ -20,7 +20,7 @@ import { User } from '../../models';
 
 @Injectable()
 export class AuthEffects {
-  public providers: Array<string>;
+  public providers: string[];
 
   constructor(
     private actions$: Actions,
